770a4f14da218b682009bb302021206186c37221
ashishgoyal192 <45450423+ashishgoyal192@users.noreply.github.com>
2018-11-29 13:31:33 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+// HTTP
+ define('HTTP_SERVER', 'https://justeat.foodesoft.com/store/');
+ define('HTTP_CATALOG', 'https://justeat.foodesoft.com/');
+
+ // HTTPS
+ define('HTTPS_SERVER', 'https://justeat.foodesoft.com/store/');
+ define('HTTPS_CATALOG', 'https://justeat.foodesoft.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/foodesoft/public_html/justeat.foodesoft.com/store/');
+define('DIR_SYSTEM', '/home/foodesoft/public_html/justeat.foodesoft.com/system/');
+define('DIR_LANGUAGE', '/home/foodesoft/public_html/justeat.foodesoft.com/store/language/');
+define('DIR_TEMPLATE', '/home/foodesoft/public_html/justeat.foodesoft.com/store/view/template/');
+define('DIR_CONFIG', '/home/foodesoft/public_html/justeat.foodesoft.com/system/config/');
+define('DIR_IMAGE', '/home/foodesoft/public_html/justeat.foodesoft.com/image/');
+define('DIR_CACHE', '/home/foodesoft/public_html/justeat.foodesoft.com/system/cache/');
+define('DIR_DOWNLOAD', '/home/foodesoft/public_html/justeat.foodesoft.com/system/download/');
+define('DIR_UPLOAD', '/home/foodesoft/public_html/justeat.foodesoft.com/system/upload/');
+define('DIR_LOGS', '/home/foodesoft/public_html/justeat.foodesoft.com/system/logs/');
+define('DIR_MODIFICATION', '/home/foodesoft/public_html/justeat.foodesoft.com/system/modification/');
+define('DIR_CATALOG', '/home/foodesoft/public_html/justeat.foodesoft.com/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'foodesof_justeat');
+define('DB_PASSWORD', 'mxB=;bNodDZq');
+define('DB_DATABASE', 'foodesof_justeat');
+define('DB_PREFIX', 'exl_');
